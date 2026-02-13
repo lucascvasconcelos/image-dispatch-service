904e73763fe92fbf645a4905da4eb3b2ddce8e65
lucasvasconcelos <consultorlucas@wba.com.br>
2026-02-12 23:29:55 -0300

    Initial commit

---
+++ src/main/java/com/br/image/image_dispatch_service/ImageDispatchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.br.image.image_dispatch_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageDispatchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageDispatchServiceApplication.class, args);
+	}
+
+}
